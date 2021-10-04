77f27e5183a2cd1bc623336f7026fd66557ab973
Usama0101 <usama.farooq@bigimmersive.com>
2021-10-05 01:34:05 +0500

    first commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const server = app.listen(
   )
 );
 
-app.get("/", (req, res) => res.send("API Running"));
+app.get("/", (req, res) => res.send(`API Running on port ${PORT}`));
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
